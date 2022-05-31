b4a83f7e0c915c9a9557244f7b238315020ad864
judgewu123 <judge0320wu@gmail.com>
2022-05-31 22:37:14 +0800

    0531 process ANOVA

--- ANOVA.R
+++ ANOVA.R
@@ -1,5 +1,5 @@
 library(lavaan)
-library(lavaan)
+library(stringr)
 library(xlsx)
 library(tidyverse)
 library(gridExtra)
@@ -12,6 +12,7 @@ library(dplyr)
 library(semPlot)
 library(Hmisc)
 library(gplots)
+library(ggiraphExtra)
 dataset <- read.xlsx("data_oil_mod.xlsx", sheetIndex = 1, encoding="UTF-8-BOM", fill = TRUE)
 
 
@@ -22,28 +23,53 @@ dataset_std_water <- scale(cbind(dataset[,11:20],dataset[,35:36]))
 pca_water <- prcomp(formula =~ temp+sal+pH+DO+Turb+NO2+NO3+NH3+PO43,   
                     data = data.frame(dataset_std_water),
                     scale = TRUE)
-
-# PC1 : -0.479 * NO3, 0.411 * temp, -0.393 * NO2
-# PC2 :  0.628 * DO, -0.438 * NH3
-# PC3 :  0.723 * pH, 
+pca_water_vars <- (pca_water$sdev)^2
+pca_water_props <- pca_water_vars / sum(pca_water_vars)
+cumulative.pca_water_props <- cumsum(pca_water_props)
+# PC1 : -0.479 * NO3, 0.411 * temp, -0.393 * NO2  # 生物代謝物
+# PC2 :  0.628 * DO, -0.438 * NH3 # 氮循環
+# PC3 :  0.723 * pH, # 酸鹼度
 # PC4 : -0.745 * Turb
 
 PC1 <- -0.479 * dataset_std_water[,7] + 0.411 * dataset_std_water[,1] - 0.393 * dataset_std_water[,6]
 PC2 <- 0.628 * dataset_std_water[,4] - 0.438 * dataset_std_water[,8]
 PC3 <- 0.723 * dataset_std_water[,3]
-PC4 <- -0.745 * dataset_std_water[,5]
+# PC4 <- -0.745 * dataset_std_water[,5]
+a <- str_c(dataset[,2] , dataset[,3], sep = ".")
+PC_score <- data.frame(cbind(dataset[,2],dataset[,3], a, dataset[,5], as.character(dataset[,6]),PC1, PC2, PC3))
+colnames(PC_score) <- c('year','month','time','season','pos', '生物代謝物', '氮循環', '酸鹼度')
 
-PC_score <- data.frame(cbind(dataset[,2],dataset[,3], as.character(dataset[,6]),PC1, PC2, PC3, as.numeric(PC4)))
-colnames(PC_score) <- c('year','month','pos', 'PC1', 'PC2', 'PC3', 'PC4')
 
-tapply(PC_score$PC1,PC_score$year,mean)
-aov_pc = aov(PC1 ~ factor(year), data = PC_score)
-summary(aov_pc)
-TukeyHSD(aov_pc)
-ggplot(TukeyHSD(aov_pc, conf.level = 0.95))
-
-aov_pc = aov(PC1 ~ factor(pos), data = PC_score)
+######## PC1 ANOVA ###################
+aov_pc = aov(生物代謝物 ~ factor(year) + factor(season), data = PC_score)
 summary(aov_pc)
 TukeyHSD(aov_pc)
 plot(TukeyHSD(aov_pc, conf.level = 0.95))
 
+aov_pc = aov(生物代謝物 ~ factor(season) + factor(pos) + factor(season) * factor(pos), data = PC_score)
+summary(aov_pc)
+TukeyHSD(aov_pc)
+plot(TukeyHSD(aov_pc, conf.level = 0.95))
+
+
+######## PC2 ANOVA ###################
+aov_pc = aov(氮循環 ~ factor(year) + factor(season), data = PC_score)
+summary(aov_pc)
+TukeyHSD(aov_pc)
+plot(TukeyHSD(aov_pc, conf.level = 0.95))
+
+aov_pc = aov(氮循環 ~ factor(season) + factor(pos) + factor(season) * factor(pos), data = PC_score)
+summary(aov_pc)
+TukeyHSD(aov_pc)
+plot(TukeyHSD(aov_pc, conf.level = 0.95))
+
+######## PC3 ANOVA ###################
+aov_pc = aov(酸鹼度 ~ factor(year) + factor(season), data = PC_score)
+summary(aov_pc)
+TukeyHSD(aov_pc)
+plot(TukeyHSD(aov_pc, conf.level = 0.95))
+
+aov_pc = aov(酸鹼度 ~ factor(season) + factor(pos) + factor(season) * factor(pos), data = PC_score)
+summary(aov_pc)
+TukeyHSD(aov_pc)
+plot(TukeyHSD(aov_pc, conf.level = 0.95))
